6967e647ec7c68ebda1cc544a040725d8c47b68a
Nathan Carter <ncarter@bentley.edu>
2023-07-11 14:14:05 -0400

    Improve UI styling

--- ui.js
+++ ui.js
@@ -4,27 +4,37 @@ import { levels } from './levels.js'
 import { Int3 } from './int3.js'
 
 let game = null
-export const setGame = g => game = g
+export const setGame = g => {
+    game = g
+    levelPicked( levels[0].name ) // fill with a default level
+}
 
 export const showWelcomeDialog = () => popup( `
-    <h1>Welcome to this experimental game.</h1>
-    <p>Press enter &rightarrow; choose a level to play</p>
-    <p>Press ? (question mark) &rightarrow; get help</p>
-    <h4>Enjoy!</h4>
+    <center>
+        <h1>Move Stuff</h1>
+        <p>an experimental game</p>
+        <input type='button' value='Choose a level' onclick='showLevelChoiceDialog()'/>
+        <input type='button' value='See help page' onclick='showHelpDialog()'/>
+    </center>
 `, game.view.domElement )
 
-const showLevelChoiceDialog = () => popup( `
-    <h1>Choose a level to play or edit:</h1>
-    <select class="focus-me" onchange="levelPicked(this);">
-        <option value="null">(choose one)</option>
-        ${levels.map(x=>'<option value="'+x.name+'">'+x.name+'</option>').join('\n')}
-    </select>
-`, game.view.domElement )
+window.showLevelChoiceDialog = () => {
+    const levelButtons = levels.map( ( level, index ) => `
+        <p><input type='button' value='${index+1}. ${level.name}'
+               onclick='levelPicked("${level.name}")'/></p>
+    ` )
+    return popup( `
+        <center>
+            <h1>Choose a level to play or edit:</h1>
+            ${levelButtons.join( '' )}
+            <p>(There are ${levels.length} levels in the game so far.)</p>
+        </center>
+    `, game.view.domElement )
+}
 
 let currentLevel = null
-window.levelPicked = ( selectElement ) => {
-    if ( selectElement.value == 'null' ) return
-    currentLevel = levels.find( level => level.name == selectElement.value )
+window.levelPicked = name => {
+    currentLevel = levels.find( level => level.name == name ) // save for reset
     game.setState( currentLevel )
     unPopup()
 }
@@ -39,122 +49,124 @@ const showLevelJSON = () => {
     popupWindow.querySelector( '#levelJSON' ).select()
 }
 
-const showHelpDialog = () => popup( `
-    <h1>Controls</h1>
-    <table style="width: 100%;" border=1 cellspacing=0 cellpadding=10>
-        <tr style="background-color: #dddddd;">
-            <th>Keyboard/mouse action</th>
-            <th>Result</th>
-        </tr>
-        <tr>
-            <td>Click and drag</td>
-            <td>Rotate viewpoint</td>
-        </tr>
-        <tr>
-            <td>Scroll mouse wheel</td>
-            <td>Zoom view in/out</td>
-        </tr>
-        <tr>
-            <td>Enter</td>
-            <td>Choose a level to edit or play</td>
-        </tr>
-        <tr>
-            <td>?</td>
-            <td>Show this help</td>
-        </tr>
-        <tr>
-            <td>Escape</td>
-            <td>Hide this help</td>
-        </tr>
-        <tr>
-            <th colspan=2 style="background-color: #eeeeee;">
-                <center>While playing a level</center>
-            </th>
-        </tr>
-        <tr>
-            <td>Click an object</td>
-            <td>Activate/use the object</td>
-        </tr>
-        <tr>
-            <td>r</td>
-            <td>Reset the current level</td>
-        </tr>
-        <tr>
-            <td>Tab</td>
-            <td>Turn on edit mode</td>
-        </tr>
-        <tr>
-            <th colspan=2 style="background-color: #eeeeee;">
-                <center>While editing a level</center>
-            </th>
-        </tr>
-        <tr>
-            <td>Arrow keys</td>
-            <td>Move 3d cursor (white wireframe) horizontally</td>
-        </tr>
-        <tr>
-            <td>w, s</td>
-            <td>Move 3d cursor (white wireframe) vertically</td>
-        </tr>
-        <tr>
-            <td>Click an object</td>
-            <td>Move the cursor there</td>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Insert a floor block</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Insert a game piece</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Insert a goal</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Insert a spinner</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>Insert a poker</td>
-        </tr>
-        <tr>
-            <td>Backspace</td>
-            <td>Delete object under cursor</td>
-        </tr>
-        <tr>
-            <td>j, k, l</td>
-            <td>Cycle features of object under cursor (though not all objects
-                have three features to adjust)</td>
-        </tr>
-        <tr>
-            <td>x</td>
-            <td>Cut item under cursor</td>
-        </tr>
-        <tr>
-            <td>c</td>
-            <td>Copy item under cursor</td>
-        </tr>
-        <tr>
-            <td>v</td>
-            <td>Paste copied/cut item to cursor location</td>
-        </tr>
-        <tr>
-            <td>Slash (/)</td>
-            <td>Show JSON code for current level</td>
-        </tr>
-        <tr>
-            <td>Tab</td>
-            <td>Turn off edit mode</td>
-        </tr>
-    </table>
+window.showHelpDialog = () => popup( `
+    <center>
+        <h1>Controls</h1>
+        <table style="width: 100%;" border=1 cellspacing=0 cellpadding=10>
+            <tr style="background-color: #dddddd;">
+                <th>Keyboard/mouse action</th>
+                <th>Result</th>
+            </tr>
+            <tr>
+                <td>Enter</td>
+                <td>Choose a level to edit or play</td>
+            </tr>
+            <tr>
+                <td>Click and drag</td>
+                <td>Rotate viewpoint</td>
+            </tr>
+            <tr>
+                <td>Scroll mouse wheel</td>
+                <td>Zoom view in/out</td>
+            </tr>
+            <tr>
+                <td>?</td>
+                <td>Show this help</td>
+            </tr>
+            <tr>
+                <td>Escape</td>
+                <td>Hide info screen/show welcome screen</td>
+            </tr>
+            <tr>
+                <th colspan=2 style="background-color: #eeeeee;">
+                    <center>While playing a level</center>
+                </th>
+            </tr>
+            <tr>
+                <td>Click an object</td>
+                <td>Activate/use the object</td>
+            </tr>
+            <tr>
+                <td>r</td>
+                <td>Reset the current level</td>
+            </tr>
+            <tr>
+                <td>Tab</td>
+                <td>Turn on edit mode</td>
+            </tr>
+            <tr>
+                <th colspan=2 style="background-color: #eeeeee;">
+                    <center>While editing a level</center>
+                </th>
+            </tr>
+            <tr>
+                <td>Arrow keys</td>
+                <td>Move 3d cursor (white wireframe) horizontally</td>
+            </tr>
+            <tr>
+                <td>w, s</td>
+                <td>Move 3d cursor (white wireframe) vertically</td>
+            </tr>
+            <tr>
+                <td>Click an object</td>
+                <td>Move the cursor there</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Insert a floor block</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Insert a game piece</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Insert a goal</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Insert a spinner</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Insert a poker</td>
+            </tr>
+            <tr>
+                <td>Backspace</td>
+                <td>Delete object under cursor</td>
+            </tr>
+            <tr>
+                <td>j, k, l</td>
+                <td>Cycle features of object under cursor (though not all objects
+                    have three features to adjust)</td>
+            </tr>
+            <tr>
+                <td>x</td>
+                <td>Cut item under cursor</td>
+            </tr>
+            <tr>
+                <td>c</td>
+                <td>Copy item under cursor</td>
+            </tr>
+            <tr>
+                <td>v</td>
+                <td>Paste copied/cut item to cursor location</td>
+            </tr>
+            <tr>
+                <td>Slash (/)</td>
+                <td>Show JSON code for current level</td>
+            </tr>
+            <tr>
+                <td>Tab</td>
+                <td>Turn off edit mode</td>
+            </tr>
+        </table>
+    </center>
 `, game.view.domElement )
 
 const generalResponses = {
     'Enter'      : showLevelChoiceDialog,
-    'Escape'     : unPopup,
+    'Escape'     : () => popupIsVisible() ? unPopup() : showWelcomeDialog(),
     '?'          : showHelpDialog,
     'Tab'        : () => game.setEditing( !game.isEditing() ),
 }
